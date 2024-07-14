31c3f8a8fa496fba8366044efbbebafb0e3daee7
Timofei <timmi202116202@gmail.com>
2024-07-14 19:50:19 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as mistake
+
+from true_math import divide as infinity
+
+result1 = mistake(69, 3)
+result2 = mistake(3, 0)
+result3 = infinity(49, 7)
+result4 = infinity(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
